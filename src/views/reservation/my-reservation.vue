baa3448168db2ff268ad3ff1cde78da33766c75d
lanto <lanto@lantodeMacBook.local>
2019-07-12 21:15:01 +0800

    7.12

--- src/views/reservation/my-reservation.vue
+++ src/views/reservation/my-reservation.vue
@@ -1,18 +1,31 @@
 <template>
 <div class="my-reservation">
+	<ul class="select">
+		<li @click="clickDatetime">预约时间<i class="fa fa-caret-down"></i></li>
+		<li>预约类型<i class="fa fa-caret-down"></i></li>
+	</ul>
 	<mt-loadmore :bottom-method="loadMore" :bottom-all-loaded="allLoaded" :autoFill="false"
 	             bottomPullText="加载更多"   ref="loadmore">
 		<ul class="list">
 			<li v-for="item in list" tag="a" @click="to(item.ORDER_ID)">
-				<div class="head"><label>预约时间：</label>
-					{{item.ORDER_DATE.substr(0,10) + " " + item.ORDER_TIME}}
-					<!--<span class="status">{{$store.state.user.unit[item.ORDER_TYPE]}}</span>-->
+				<div class="head">
+					<label>{{item.name}}</label>
+					<span class="status">{{item.status}}</span>
 				</div>
-				<p><label>指派公司：</label>{{item.TENANT_NAME}}</p>
-				<p><label>服务内容：</label>{{$store.state.user.unit[item.REPAIR_TYPE]}}</p>
+				<p><label>预约时间：</label>{{item.time}}</p>
+				<p><label>车牌号：</label>{{item.card}}</p>
 			</li>
 		</ul>
 	</mt-loadmore>
+
+	<mt-datetime-picker
+			ref="datetime"
+			v-model="date"
+			type="date"
+			year-format="{value} 年"
+			month-format="{value} 月"
+			date-format="{value} 日">
+	</mt-datetime-picker>
 </div>
 </template>
 
@@ -21,14 +34,24 @@ export default {
 	name: "my-reservation",
 	data(){
 		return{
-			list:[],
+			list:[
+				{
+					name: '洗车',
+					time: '2019-05-23 11:38',
+					card: '沪AL59A1',
+					status: '待处理'
+				}
+			],
+			date: '',
+			type:'',
 			page: 1,
 			total: 0,
 			allLoaded: false,
+
 		}
 	},
 	mounted(){
-		this.getList(false)
+		// this.getList(false)
 	},
 	methods:{
 	    to(id){
@@ -61,6 +84,9 @@ export default {
 			this.page++
 			this.getList(true)
 		},
+		clickDatetime(){
+	    	this.$refs.datetime.open()
+		}
 	}
 }
 </script>
@@ -69,33 +95,68 @@ export default {
 .my-reservation{
 	height: 100vh;
 	overflow: auto;
-	background-color: #F3F3F3;
-	.list{
+	/*background-color: #F3F3F3;*/
+	.select{
+		position: fixed;
+		top: 0;
+		left: 0;
+		border-bottom: 1px solid #EDEDED;
+		width: 100%;
+		padding: 0 10px;
+		background-color: white;
 		li{
-			margin-top: 10px;
-			padding: 0 15px 10px;
+			color: #666666;
+			margin-right: 10px;
+			line-height: 40px;
+			float: left;
+			position: relative;
+			font-size: 14px;
+			i{
+				margin-left: 5px;
+			}
+		}
+	}
+	.list{
+		margin-top: 40px;
+		padding-left: 15px;
+		li{
 			background-color: white;
+			border-bottom: 1px solid #EDEDED;
+			padding: 10px 0;
 			.head{
-				height: 41px;
-				line-height: 40px;
+				line-height: 30px;
 				font-size: 14px;
-				border-bottom: 1px solid #D9D9D9;
+				font-weight: 600;
 				color: #333333;
-				margin-bottom: 10px;
 				label{
 					color: #666666;
 				}
 				.status{
 					float: right;
 					color: #FF9738;
+					margin-right: 15px;
+					padding-right: 10px;
+					position: relative;
+					&:after{
+						content: '';
+						position: absolute;
+						right: 0;
+						top: 50%;
+						border-right: 1px solid;
+						border-bottom: 1px solid;
+						width: 8px;
+						height: 8px;
+						color: #999;
+						transform: translateY(-50%) rotate(-45deg);
+					}
 				}
 			}
 			p{
-				font-size: 12px;
+				font-size: 13px;
 				line-height: 22px;
-				color: #333333;
+				color: #666666;
 				label{
-					color: #999999;
+					/*color: #999999;*/
 				}
 			}
 		}
--- src/views/reservation/reservation-detail.vue
+++ src/views/reservation/reservation-detail.vue
@@ -1,13 +1,69 @@
 <template>
-
+<div class="reservation-detail">
+	<Form :class="['common-form']" :model="info"
+	      :label-width="80" label-position="left" ref="form">
+		<!--<FormItem label="门店名称">-->
+		<!--<span class="line">{{TENANT_NAME}}</span>-->
+		<!--</FormItem>-->
+		<FormItem label="服务门店" prop="appointmentTime">
+			<span class="text">{{info.shop}}</span>
+		</FormItem>
+		<FormItem label="门店地址" prop="appointmentTime">
+			<span class="select">{{info.add}}</span>
+		</FormItem>
+		<FormItem label="门店电话" prop="appointmentTime">
+			<span class="text">{{info.tel}}</span>
+			<a class="tel">
+				<i class="fa fa-phone"></i>
+			</a>
+		</FormItem>
+		<FormItem label="预约时间" prop="appointmentTime">
+			<span class="text">{{info.shop}}</span>
+		</FormItem>
+		<FormItem label="预约类型" prop="appointmentTime">
+			<span class="text">{{info.shop}}</span>
+		</FormItem>
+		<FormItem label="车牌号" prop="appointmentTime">
+			<span class="text">{{info.shop}}</span>
+		</FormItem>
+		<FormItem label="预约单号" prop="appointmentTime">
+			<span class="text">{{info.shop}}</span>
+		</FormItem>
+		<FormItem label="状态" prop="appointmentTime">
+			<span class="text">{{info.shop}}</span>
+		</FormItem>
+	</Form>
+</div>
 </template>
 
 <script>
-	export default {
-		name: "reservation-detail"
+export default {
+	name: "reservation-detail",
+	data(){
+		return{
+			info:{
+				shop: '小明汽车快修',
+				add: '淞虹路207号明基商务广场C栋',
+				tel: '021-55678906'
+			}
+		}
 	}
+}
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.reservation-detail{
+	.common-form{
+		.tel{
+			position: absolute;
+			top: 50%;
+			right: 15px;
+			transform: translateY(-50%);
+			display: inline-block;
+			width: auto;
+			margin: 0;
+			font-size: 22px;
+		}
+	}
+}
 </style>
